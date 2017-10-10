a57455d74ee24aa87b8112463c7f62d26f981098
Jhonti <32675453+Jhonti@users.noreply.github.com>
2017-10-10 14:33:29 +0100

    commented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,20 +9,29 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+    	//take input from user
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter an email id...");
         String id = reader.readLine();
 
+        // Construct the url and open stream
         String url = "http://www.ecs.soton.ac.uk/people/" + id;
         URL page = new URL(url);
         BufferedReader pagereader = new BufferedReader(new InputStreamReader(page.openStream()));
 
+
+        /*
+        Go through each line of the html until <title> is found,
+        stop on that line
+         */
         String line;
         while((line = pagereader.readLine()) != null){
             if (line.contains("<title>")){
                 break;
             }
         }
+
+        // Define start/end indexes, name contained between
         int finishindex = line.indexOf("|");
         int startindex =  line.indexOf(">") + 1;
 
